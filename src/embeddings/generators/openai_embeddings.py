930f3b2efa8567b917ee2961abdf7fc42d74150f
Siva <sivakesavakuchi@licious.com>
2023-10-12 19:58:01 +0530

    fifth draft

--- src/embeddings/generators/openai_embeddings.py
+++ src/embeddings/generators/openai_embeddings.py
@@ -1,12 +1,10 @@
 from typing import List
-from dotenv import load_dotenv
 from llama_index.embeddings import OpenAIEmbedding
 import os
 from utils.log_utils import kclogger
 
 class OpenAIEmbeddings:
     def __init__(self):
-        load_dotenv()
         openaiApiKey = os.environ.get('OPENAI_API_KEY')
         kclogger.info(f"OpenAIEmbeddings::init openaiApiKey: {openaiApiKey}")
         self.model = OpenAIEmbedding(api_key=openaiApiKey)
